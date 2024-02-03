ebe16088373d39fc898abea43f8ab1161252b8cf
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-02-02 20:40:59 -0600

    Se agrego la funcion de editar y de eliminar al todoList

--- assets/scripts/todoList.js
+++ assets/scripts/todoList.js
@@ -24,7 +24,7 @@ function validarFormulario(e){
     }
 
     if(editando){
-        //editarTarea();
+        editarTarea();
         editando = false;
     }else{
         objTarea.id = Date.now();
@@ -38,10 +38,20 @@ function validarFormulario(e){
 function agregarTarea(){
     listaTareas.push({...objTarea});
 
-    mostrarEmpleados();
+    mostrarTareas();
+    formulario.reset();
+    limpiarObjeto();
 }
 
-function mostrarEmpleados(){
+function limpiarObjeto(){
+    objTarea.id = '';
+    objTarea.nombre = '';
+    objTarea.descripcion = '';
+}
+
+function mostrarTareas(){
+
+    limpiarHTML();
     const divTareas = document.querySelector('.div-tareas');
     listaTareas.forEach( tarea =>{
         const {id, nombre, descripcion} = tarea;
@@ -51,13 +61,13 @@ function mostrarEmpleados(){
         parrafo.dataset.id= id;
 
         const editarBoton = document.createElement('button');
-        //editarBoton.onclick = () => cargarTarea(tarea);
+        editarBoton.onclick = () => cargarTarea(tarea);
         editarBoton.textContent = 'Editar';
         editarBoton.classList.add('btn', 'btn-editar');
         parrafo.append(editarBoton);
 
         const eliminarBoton = document.createElement('button');
-        //eliminarBoton.onclick = () => eliminarTarea(tarea);
+        eliminarBoton.onclick = () => eliminarTarea(tarea);
         eliminarBoton.textContent = 'Eliminar';
         eliminarBoton.classList.add('btn', 'btn-eliminar');
         parrafo.append(eliminarBoton);
@@ -71,3 +81,49 @@ function mostrarEmpleados(){
 
     );
 }
+
+function cargarTarea(tarea){
+    const {id, nombre, descripcion} = tarea;
+    nombreInput.value = nombre;
+    descripcionInput.value = descripcion;
+    objTarea.id = id;
+    formulario.querySelector('button[type="submit"]').textContent = 'Actualizar';
+    editando = true;
+}
+
+function editarTarea(){
+    objTarea.nombre = nombreInput.value;
+    objTarea.descripcion = descripcionInput.value;
+
+    listaTareas.map(tarea =>{
+        if(tarea.id === objTarea.id){
+            tarea.id = objTarea.id;
+            tarea.nombre = objTarea.nombre;
+            tarea.descripcion = objTarea.descripcion;
+        }
+    });
+
+    limpiarHTML();
+    mostrarTareas();
+
+    formulario.reset();
+
+    formulario.querySelector('button[type="submit"]').textContent = 'Agregar';
+    editando = false;
+}
+
+function eliminarTarea(id){
+    listaTareas = listaTareas.filter(tarea => tarea.id !== id);
+    limpiarHTML();
+    mostrarTareas();
+}
+
+
+function limpiarHTML(){
+    const divTareas = document.querySelector('.div-tareas');
+    while(divTareas.firstChild){
+        divTareas.removeChild(divTareas.firstChild);
+    }
+}
+
+
